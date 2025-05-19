5f85f7891f83913141f852ab756872beb1a3ebd1
Fallou SY <fallousy72@gmail.com>
2025-05-19 12:47:41 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `<h1>Welcome to {{ pokemonList[0] }}`
+})
+export class AppComponent implements OnInit {
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  
+}
